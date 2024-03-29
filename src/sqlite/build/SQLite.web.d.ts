43446d70d249584a3a1e1120039f5ebf60f29d52
Adam Esbjörnsson <adamesbj@gmail.com>
2024-03-29 23:20:50 +0100

    cleanup sqlite

--- src/sqlite/build/SQLite.web.d.ts
+++
@@ -1,3 +0,0 @@
-import { DatabaseCallback } from '../src/SQLite.types';
-export declare function openDatabase(name: string, version?: string, description?: string, size?: number, callback?: DatabaseCallback): import("../src/SQLite.types").Database;
-//# sourceMappingURL=SQLite.web.d.ts.map
--- src/sqlite/build/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from '../src/SQLite';
-export * from '../src/SQLite.types';
-//# sourceMappingURL=index.d.ts.map
